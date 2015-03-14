afc8967fb1fe6f927f52e04897fd730173f1ce8d
Jason Orendorff <jason.orendorff@gmail.com>
2015-03-13 20:33:29 -0500

    Make Reversi saner to play; flip the y axis; change the UI (but it's still so bad)

--- jorendorff+haskell/Reversi.hs
+++ jorendorff+haskell/Reversi.hs
@@ -3,7 +3,7 @@
 import Minimax
 import Data.Array
 import Vs
-import Data.List(intercalate, elemIndex)
+import Data.List(intersperse, intercalate, elemIndex)
 import Data.Char(toLower)
 
 --- Reversi
@@ -20,40 +20,45 @@ instance Show Square where
   show White = "O"
   show Empty = "."
 
-data Reversi = Reversi (Array Int Square)
+data Reversi = Reversi (Array Int Square) Square
 
 letters = "abcdefgh"
 size = length letters  -- must be even, or the starting board is weird and the game can't be played
 sizeSq = size * size
 
 data ReversiMove = Pass | MoveAt (Int, Int)
+  deriving Eq
 
 instance Show ReversiMove where
   show Pass = "pass"
-  show (MoveAt (x, y)) = [letters !! x] ++ show y
+  show (MoveAt (x, y)) = [letters !! x] ++ show (y + 1)
 
 instance Read ReversiMove where
   readsPrec _ ('p' : 'a' : 's' : 's' : etc) = [(Pass, etc)]
   readsPrec _ (letter : digits) = case elemIndex (toLower letter) letters of
     Just x -> do (y, etc) <- readsPrec 0 digits
-                 return (MoveAt (x, y), etc)
+                 if 1 <= y && y <= size
+                   then return (MoveAt (x, y - 1), etc)
+                   else []
     _ -> []
 
 pointToIndex (x, y) = y * size + x
 indexToPoint k = (k `mod` size, k `div` size)
 
 instance Show Reversi where
-  show (Reversi arr) = concat
+  show (Reversi arr _) = concat
     [intercalate " " [show (arr ! pointToIndex (x, y))
-                     | x <- [0 .. size - 1]] ++ "\n"
-    | y <- [0 .. size - 1]]
+                     | x <- [0 .. size - 1]] ++ "  " ++ show (y + 1) ++ "\n"
+    | y <- reverse [0 .. size - 1]] ++ intersperse ' ' letters ++ "\n"
 
 dirs = [(-1, -1), (-1, 0), (-1, 1),
         (0, -1),           (0, 1),
         (1, -1),  (1, 0),  (1, 1)]
 
-captureIndexes arr pt =
+captureIndexes arr me pt =
   let
+    you = flipSquare me
+
     -- (ray dir point) computes the list of all indices of game squares in
     -- the ray starting at point and extending in the direction dir, not
     -- including point itself.
@@ -67,9 +72,9 @@ captureIndexes arr pt =
     -- the direction dir from pt, that have black pieces that will be flipped
     -- if we move at pt.
     rayFlips dir = case ray dir pt of
-      xxs@(x : xs) | arr ! x == Black ->
-        case span (\ i -> arr ! i == Black) xxs of
-          (captured, further : _) | arr ! further == White -> captured
+      xxs@(x : xs) | arr ! x == you ->
+        case span (\ i -> arr ! i == you) xxs of
+          (captured, further : _) | arr ! further == me -> captured
           _ -> []
       _ -> []
 
@@ -77,17 +82,16 @@ captureIndexes arr pt =
     -- the list of all indices of black pieces that will be flipped
     concatMap rayFlips dirs
 
-reversiApplyMove (Reversi arr) Pass = Reversi (fmap flipSquare arr)
-reversiApplyMove (Reversi arr) (MoveAt pt) =
-  let places = pointToIndex pt : captureIndexes arr pt
-      arr' = arr // [(j, White) | j <- places]
-      arr'' = fmap flipSquare arr'
-  in Reversi arr''
+reversiApplyMove (Reversi arr turn) Pass = Reversi arr (flipSquare turn)
+reversiApplyMove (Reversi arr turn) (MoveAt pt) =
+  let places = pointToIndex pt : captureIndexes arr turn pt
+      arr' = arr // [(j, turn) | j <- places]
+  in Reversi arr' (flipSquare turn)
 
 -- Super lame implementation, no elegance whatsoever
-reversiMoves (Reversi arr) =
+reversiMoves (Reversi arr turn) =
   let
-    canCapture k = not $ null $ captureIndexes arr $ indexToPoint k
+    canCapture k = not $ null $ captureIndexes arr turn $ indexToPoint k
     emptySquares = filter (\ x -> arr ! x == Empty) $ [0 .. sizeSq - 1]
     -- All legal capturing moves.
     capturingMoves = map (MoveAt . indexToPoint) $ filter canCapture emptySquares
@@ -104,23 +108,29 @@ count x lst = length $ filter (== x) lst
 
 instance Game Reversi where
   type Move Reversi = ReversiMove
-  start = Reversi $ listArray (0, sizeSq - 1) $ (
+  start =
     let blank = take ((size * (size - 1) - 2) `div` 2) $ repeat Empty
         mid = take (size - 2) $ repeat Empty
-    in blank ++ [White, Black] ++ mid ++ [Black, White] ++ blank)
+        squareList = blank ++ [White, Black] ++ mid ++ [Black, White] ++ blank
+    in Reversi (listArray (0, sizeSq - 1) squareList) Black
 
   moves = reversiMoves
 
   applyMove = reversiApplyMove
 
-  scoreFinishedGame (Reversi arr) =
-    let stuff = elems arr
-    in fromIntegral (count Black stuff - count White stuff)
+  scoreFinishedGame (Reversi arr you) =
+    let squares = elems arr
+        me = flipSquare you
+    in fromIntegral (count me squares - count you squares)
 
-
-heuristic (Reversi arr) =
-  let stuff = elems arr
-  in 0.01 * fromIntegral (count Black stuff - count White stuff)
+heuristic (Reversi arr you) =
+  let squares = elems arr
+      me = flipSquare you
+      yours = count you squares
+      mine = count me squares
+      delta = fromIntegral (mine - yours)
+  in if yours + mine < 40 then -0.01 * delta           -- opening: minimize
+     else 0.01 * delta                                 -- endgame: maximize
 
 reversiSmartyPantsAI = bestMoveWithDepthLimit heuristic 4
 
