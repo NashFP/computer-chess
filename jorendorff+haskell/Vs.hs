0fc2e4e6aff1e17ee1f39b7fb351f0aee02356f0
Jason Orendorff <jason.orendorff@gmail.com>
2015-03-13 20:31:54 -0500

    Much better command-line UI -- less fragile, more user-friendly.

--- jorendorff+haskell/Vs.hs
+++ jorendorff+haskell/Vs.hs
@@ -2,8 +2,30 @@ module Vs(playHumanVsComputer) where
 
 import Minimax(applyMove, moves, scoreFinishedGame)
 import System.IO(hFlush, stdout)
+import Data.Char(isSpace)
 
---- Command-line UI to play against a perfect computer opponent ---------------
+--- Command-line UI to play against a computer opponent -----------------------
+
+inputOneOf :: (Show a, Read a, Eq a) => [a] -> String -> IO (Maybe a)
+inputOneOf options prompt = do
+  putStr prompt
+  hFlush stdout
+  line <- getLine
+  if line == "?"
+    then retry ("options: " ++ show options)
+    else if line == "q"
+         then return Nothing
+         else do
+           case reads line of
+             (value, rest) : _ ->
+               if all isSpace rest
+               then if elem value options
+                    then return $Just value
+                    else retry $ show value ++ " is not an option (enter ? to show all options)"
+               else retry "i didn't understand all of that"
+             _ -> retry "i didn't understand that"
+  where retry msg = putStrLn msg >> inputOneOf options prompt
+
 
 --playHumanVsComputer :: (Game g, tons of other stuff) => g -> IO ()
 playHumanVsComputer selectMove start = do
@@ -28,20 +50,18 @@ playHumanVsComputer selectMove start = do
 
     --humansTurn :: g -> IO ()
     humansTurn board = do
-      putStrLn $ "your legal moves are: " ++ show (moves board)
-      putStr "your turn> "
-      hFlush stdout
-      line <- getLine
-      if line == "q" then return () else do
-        let move = read line
-        let newBoard = applyMove board move
-        putStrLn $ show newBoard
-        case moves newBoard of
-          [] -> do  -- computer has no moves; game is over
-            putStrLn "game over"
-            putStrLn $ case scoreFinishedGame newBoard of
-              0 -> "it's a tie"
-              x | x > 0 -> "you win"
-              _ -> "i win"
-          _ ->
-            computersTurn newBoard
+      maybeMove <- inputOneOf (moves board) "your turn> "
+      case maybeMove of
+        Nothing -> return ()  -- user typed "q" to quit
+        Just move -> do
+          let newBoard = applyMove board move
+          putStrLn $ show newBoard
+          case moves newBoard of
+            [] -> do  -- computer has no moves; game is over
+              putStrLn "game over"
+              putStrLn $ case scoreFinishedGame newBoard of
+                0 -> "it's a tie"
+                x | x > 0 -> "you win"
+                _ -> "i win"
+            _ ->
+              computersTurn newBoard
