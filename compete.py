95a756c8ac211adb676ba1b855e7c1f73352d1d1
Jason Orendorff <jason.orendorff@gmail.com>
2015-03-15 08:47:51 -0500

    Completely rearrange microchess.cpp. ***OOP code don't look***

--- compete.py
+++ compete.py
@@ -4,79 +4,84 @@
 
 import sys, subprocess, re, threading, queue, time
 
-def make_program_output_queue(color, f):
-    q = queue.Queue()
-    def reader():
-        for line in f:
+class ChessPlayingProcess:
+    def __init__(self, color, cmd):
+        self.color = color
+
+        # The sleep calls sprinkled throughout this program are a little hack
+        # so that output from the two child processes usually isn't interleaved
+        # line-by-line on your terminal. When that happens with two chessboards
+        # it can be pretty hard to tell what's going on.
+        time.sleep(0.5)
+        self.process = subprocess.Popen(cmd,
+                                        bufsize=1, universal_newlines=True,
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        self.q = queue.Queue()
+        threading.Thread(target=self._reader).start()
+
+    def _reader(self):
+        for line in self.process.stdout:
             line = line.rstrip("\n")
-            print(color + "> " + line)
-            q.put(line)
-        q.put(None)
-    threading.Thread(target=reader).start()
-    return q
+            print(self.color + "> " + line)
+            self.q.put(line)
+        self.q.put(None)
 
-def common(q):
-    """ Read lines from q until one matches the pattern. Return the move. """
+    def _read_move(self):
+        """ Read lines from q until one matches the pattern. Return the move. """
 
-    time.sleep(0.5)
-    should_close = True
+        time.sleep(0.5)
+        should_close = True
 
-    while True:
-        line = q.get()
-        if line is None:
-            break
+        while True:
+            line = self.q.get()
+            if line is None:
+                break
 
-        line = line.rstrip()
-        if line == "" or line == ">" or line.startswith(("    ", "\t")):
-            continue
+            line = line.rstrip()
+            if line == "" or line == ">" or line.startswith(("    ", "\t")):
+                continue
 
-        m = re.match(r'^(?:.*:)?\s*([a-h][1-8][a-h][1-8]|O-O|O-O-O|resign)$', line)
-        if m is not None:
-            should_close = False
-            return m.group(1)
-        print("*> WARNING: ignoring the line {!r} since it doesn't match".format(line))
+            m = re.match(r'^(?:.*:)?\s*([a-h][1-8][a-h][1-8]|O-O|O-O-O|resign)$', line)
+            if m is not None:
+                should_close = False
+                return m.group(1)
+            print("*> WARNING: ignoring the line {!r} since it doesn't match".format(line))
 
-    # End of input without a move. Either the game is over, and our reply
-    # doesn't matter; or it's a bug and we treat as resignation.
-    return "resign"
+        # End of input without a move. Either the game is over, and our reply
+        # doesn't matter; or it's a bug and we treat as resignation.
+        return "resign"
 
+    def send(self, cmd):
+        time.sleep(0.5)
+        print("*> sending {!r} to {}".format(cmd, self.color))
+        self.process.stdin.write(cmd + "\n")
 
-def microchess_as_white():
-    time.sleep(0.5)
-    white = subprocess.Popen(["jorendorff+cpp/microchess"],
-                             bufsize=1, universal_newlines=True,
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    q = make_program_output_queue('W', white.stdout)
-    firstMove = [True]
+class MicrochessWhite(ChessPlayingProcess):
+    def __init__(self):
+        ChessPlayingProcess.__init__(self, 'W', ["jorendorff+cpp/microchess"])
+        self.firstMove = True
 
-    def play(move):
-        if firstMove[0]:
+    def play(self, move):
+        if self.firstMove:
             assert move is None
-            firstMove[0] = False
+            self.firstMove = False
         else:
             # TODO - if this move is en passant or castling, we could paper over
             # Microchess's simplicity here; if promotion, we should probably
             # forfeit.
-            time.sleep(0.5)
-            white.stdin.write(move + "\n")
+            self.send(move)
 
-        time.sleep(0.5)
-        white.stdin.write("p\n")  # hey microchess it is your turn
-        return common(q)
+        self.send("p")  # hey microchess it is your turn
+        return self._read_move()
 
-    return play
+class JorendorffHaskellBlack(ChessPlayingProcess):
+    def __init__(self):
+        ChessPlayingProcess.__init__(self, 'B', ["jorendorff+haskell/Chess"])
 
-def jorendorff_haskell_as_black():
-    time.sleep(0.5)
-    black = subprocess.Popen(["jorendorff+haskell/Chess"],
-                             bufsize=1, universal_newlines=True,
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    q = make_program_output_queue('B', black.stdout)
-
-    def play(move):
-        time.sleep(0.5)
-        black.stdin.write(move + "\n")
-        reply = common(q)
+    def play(self, move):
+        self.send(move)
+        reply = self._read_move()
         if len(reply) == 4:
             # horrible kludge: haskell makes all its moves backwards,
             # so turn it around manually here (easier than doing the same thing in haskell)
@@ -89,17 +94,14 @@ def jorendorff_haskell_as_black():
             print("*> reply translated to:", reply)
         return reply
 
-    return play
-
 def play(white, black):
     lastMove = None
     while 1:
-        lastMove = white(lastMove)
+        lastMove = white.play(lastMove)
         if lastMove == "resign":
             break
-        lastMove = black(lastMove)
+        lastMove = black.play(lastMove)
         if lastMove == "resign":
             break
 
-
-play(microchess_as_white(), jorendorff_haskell_as_black())
+play(MicrochessWhite(), JorendorffHaskellBlack())
