382e3eb9b38be11ed0524b2ae798c7286b910e6a
Jason Orendorff <jason.orendorff@gmail.com>
2015-03-24 11:31:32 -0500

    jorendorff+haskell: Speed up the heuristic by doing fewer int-to-float conversions.

--- jorendorff+haskell/ChessAI.hs
+++ jorendorff+haskell/ChessAI.hs
@@ -12,22 +12,19 @@ heuristic1 g = 0.05 * fromIntegral (popCount (wholeSuite $ black g) - popCount (
 
 -- Now we're at the point where we give the pieces values.
 heuristic g =
-  let count field side = fromIntegral $ popCount $ field side
-
-      -- This code is complicated slightly by our trick of representing a queen
-      -- as a bishop and a rook.
-      justBishops side = bishops side .&. complement (rooks side)
-      justRooks side = rooks side .&. complement (bishops side)
-      queens side = bishops side .&. rooks side
-
-      -- The scores here are from Wikipedia, which lists a dozen or more
+  let -- The scores here are from Wikipedia, which lists a dozen or more
       -- scoring systems to choose from. This one is credited to Hans Berliner.
-      total side = count pawns side
-                   + 3.2 * count knights side
-                   + 3.33 * count justBishops side
-                   + 5.1 * count justRooks side
-                   + 8.80 * count queens side
-      diff = 0.001 * (total (black g) - total (white g))
+      --
+      -- This code is complicated slightly by our trick of representing a queen
+      -- as a bishop and a rook. For speed, we do not bother eliminating queens
+      -- when counting the number of bishops/rooks, but rather count queens as
+      -- both bishops and rooks, and then add a bit *more*...
+      total side =   100 * popCount (pawns side)
+                   + 320 * popCount (knights side)
+                   + 333 * popCount (bishops side)
+                   + 510 * popCount (rooks side)
+                   +  37 * popCount (bishops side .&. rooks side)
+      diff = 0.00001 * fromIntegral (total (black g) - total (white g))
   in case whoseTurn g of
        White -> diff
        Black -> -diff
