ea552291074b7acb4931b9596af616879e098453
Jason Orendorff <jason.orendorff@gmail.com>
2015-03-24 12:21:53 -0500

    jorendorff+haskell: Silence a deprecation warning about parPair.

--- jorendorff+haskell/Minimax.hs
+++ jorendorff+haskell/Minimax.hs
@@ -6,14 +6,14 @@ module Minimax(Game, Move, start, moves, applyMove, scoreFinishedGame, bestMove,
 import Data.List(maximumBy)
 
 -- Hi! If this import doesn't work for you, you just need to `cabal install parallel`. :)
-import Control.Parallel.Strategies(parMap, parPair, r0, rseq)
+import Control.Parallel.Strategies(parMap, parTuple2, r0, rseq)
 -- Or you can just delete it, and change the following line of code to:
 --     pmap = map
 --
 -- pmap computes exactly the same thing as map, when it works. There are just two differences:
 -- 1.  pmap can only produce lists of pairs.
 -- 2.  pmap spreads out the computation across multiple CPU cores when you run with +RTS -N4.
-pmap = parMap (parPair r0 rseq)
+pmap = parMap (parTuple2 r0 rseq)
 
 -- Takes a scoring function and returns the element of the list that has the
 -- greatest score. This applies the scoring function once to each list element.
